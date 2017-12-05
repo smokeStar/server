f673eaf2191ee845fe69adc600c9a51f62c9e3cd
smokeStar <1787176370@qq.com>
2017-12-05 09:54:54 +0800

    test

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
 //---------- 开启服务v17.11.23 ----------------------------
 
-const api        = require ('./api')
-    , fs         = require ('fs')
+const fs         = require ('fs')
     , path       = require ('path')
-    , bodyParser = require ('body-parser')
-
     , express    = require ('express')
-    , cors       = require('cors')
     , app        = express();
 
-// 自定义中间件
-let allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials','true');
-  next();
-};
-
-// 中间件的使用
-app.use(allowCrossDomain);   // 允许跨域
-app.use(bodyParser.json()); // 解析body数据
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(api);
-app.use(express.static(path.resolve(__dirname,'../dist')));
 
 app.get('*', function(req, res){
-  const html = fs.readFileSync(path.resolve(__dirname,'../dist/index.html'),'utf-8');
+  const html = fs.readFileSync('./index.html','utf-8');
   res.send(html);
 });
 app.listen(8088);
